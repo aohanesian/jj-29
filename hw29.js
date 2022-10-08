d1e544b8ae1d5f93f9d982e2cffb5adc08500d8f
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-10-08 05:01:48 +0300

    commit

--- hw29.js
+++ hw29.js
@@ -1,20 +1,17 @@
 `use strict`;
 
-arr2 = [];
 function getRandomInRange(min, max) {
-    while (arr2.length < max) {
+    let memory = [];
+    let final = [];
+    while (final.length < max) {
         let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        let found = false;
-        for (let i = 0; i < arr2.length; i++) {
-            if (arr2[i] === randomNumber) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            arr2[arr2.length] = randomNumber;
-        }
-    }console.log(arr2);
+        if (final.includes(randomNumber)) {
+            memory.push(randomNumber)
+        } else final.push(randomNumber)
+    }
+    console.log(memory)
+    return final;
 }
 
-getRandomInRange(1, 100);
+let generateRandomArr = getRandomInRange(1, 100);
+console.log(generateRandomArr);
